069c943bef00a291c9663e9ef2f26b49279ac5dd
Antonia Carrizo <aacarrizo@miuandes.cl>
2022-09-15 07:32:37 -0300

    chang8

--- project2_app/project2_app/settings.py
+++ project2_app/project2_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,10 +132,8 @@ SASS_PROCESSOR_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
-import django_heroku
 django_heroku.settings(locals())
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT='staticfiles'
 
