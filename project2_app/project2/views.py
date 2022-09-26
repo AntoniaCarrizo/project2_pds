9fae19bec4cbe80c021dfe2a96e310f2525826e2
Antonia Carrizo <aacarrizo@miuandes.cl>
2022-09-26 08:22:41 -0300

    intento1000

--- project2_app/project2/views.py
+++ project2_app/project2/views.py
@@ -2,9 +2,12 @@ from queue import PriorityQueue
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View 
-from .forms import *
-
+from .forms import UserRegisterForm
+from .forms import TaskForm
+from django.contrib.auth.forms import  UserCreationForm
 from project2.models import Task, Diagram, Fuerza, Momento, Apoyo
+from django.contrib.auth.models import  User
+from django.contrib import messages
 
 # Create your views here.
 class EditView(View):
@@ -26,6 +29,7 @@ class EditView(View):
     def post(self, request, task_id):
         print(request.POST.get('ident'))
         task = Task.objects.get(id=task_id)
+        
         if request.POST.get('ident') == 'description':
             new_description = request.POST.get('description')
             task.description = new_description
@@ -35,7 +39,7 @@ class EditView(View):
             task.dcl = dcl_update
             task.save()
             
-        return HttpResponse()
+        return render(request, 'edit_task.html')
 
 
 def all_tasks(request):
@@ -43,6 +47,16 @@ def all_tasks(request):
     tasks_list = Task.objects.all()
     context = {'tasks_list':tasks_list}
     return render(request, 'all_tasks.html', context)
+
+def users(request):
+    users_list = User.objects.all()
+    context = {'users_list':users_list}
+    return render(request, 'users.html', context)
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
 
 def newTask(request):
     new_task = Task()
@@ -63,4 +77,25 @@ def task_view(request):
     else: 
         form = TaskForm() 
     return render(request, 'edit_task.html', {'form' : form}) 
+
+def register(request):
+    if request.method == 'POST': 
+        form = UserCreationForm(request.POST) 
+
+        if form.is_valid(): 
+            form.save()
+            return redirect('login')
+    else: 
+        form = UserRegisterForm()
+    context = {'form' : form}  
+    return render(request,'register.html', context)
+
+def delete(request, id_task):
+    task = Task.objects.get(pk=id_task)
+    task.delete()
+    tasks = Task.objects.all()
+    return render(request, 'all_tasks.html', {"tasks_list":tasks, "mensaje": "OK"})
+
+
+
   
